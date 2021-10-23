99428ca65d6a399848f9c1ae29247f139f566280
Christian <ckim466@gatech.edu>
2021-10-23 17:20:50 -0400

    aaaaaah

--- popup.js
+++ popup.js
@@ -1,24 +1,61 @@
 
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {createDiv: true}, function(response) {
+      console.log("message sent");
+  });
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-window.onload=function() {
-changeColor.addEventListener("click", async () => {
-    console.log("button pressed");
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: on,
-    });
-  });
- 
-  function on() {
-    document.getElementById("overlay").style.display = "block";
-  }
-}
+//let startButton = document.getElementById("startButton");
+
+  // startButton.addEventListener("click", async () => {
+  //   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  //   console.log('in popoup');
+  //   chrome.scripting.executeScript({
+  //     target: { tabId: tab.id },
+  //     function: displayOverlay,
+  //   });
+  // });
+
+
+  //   function displayOverlay() {
+  //     let iframe = document.createElement('iframe'); 
+  //     iframe.style.background = "green";
+  //     iframe.style.height = "100%";
+  //     iframe.style.width = "0px";
+  //     iframe.style.position = "fixed";
+  //     iframe.style.top = "0px";
+  //     iframe.style.right = "0px";
+  //     iframe.style.zIndex = "9000000000000000000";
+  //     iframe.src = chrome.extension.getURL("popup.html");
+  //     document.body.appendChild();
+  //   };
+
+// chrome.runtime.onMessage.addListener(function(msg, sender){
+//     if(msg == "toggle"){
+//         toggle();
+//     }
+// })
+
+// var iframe = document.createElement('iframe'); 
+// iframe.style.background = "green";
+// iframe.style.height = "100%";
+// iframe.style.width = "0px";
+// iframe.style.position = "fixed";
+// iframe.style.top = "0px";
+// iframe.style.right = "0px";
+// iframe.style.zIndex = "9000000000000000000";
+// iframe.frameBorder = "none"; 
+// iframe.src = chrome.extension.getURL("popup.html")
+
+// document.body.appendChild(iframe);
+
+// function toggle(){
+//     if(iframe.style.width == "0px"){
+//         iframe.style.width="400px";
+//     }
+//     else{
+//         iframe.style.width="0px";
+//     }
+// }
+
   
